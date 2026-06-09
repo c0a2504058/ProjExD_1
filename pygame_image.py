f0d93b136b3b0dcb68a62c304767477c22918c26
c0a2504058 <c0a2504058@edu.teu.ac.jp>
2026-06-09 16:55:59 +0900

    演習１：流れるこうかとん

--- pygame_image.py
+++ pygame_image.py
@@ -15,7 +15,7 @@ def main():
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    
+
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -29,8 +29,8 @@ def main():
         if key_list[pg.K_LEFT]:
             kk_rct.move_ip(-1,0)
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip(+1,0)
-
+            kk_rct.move_ip(+2,0)
+        kk_rct.move_ip(-1,0)
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img_flippd,[-x+1600, 0])
